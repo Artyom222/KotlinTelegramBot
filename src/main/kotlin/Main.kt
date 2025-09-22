5be0fb64aa33077b7971bed3e7b656658e3e1408
APecherkin <pech.artyom2@yandex.ru>
2025-09-22 21:31:47 +0500

    KTB-03

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,15 +2,24 @@ package org.example
 
 import java.io.File
 
+data class Word(
+    val original: String,
+    val translate: String,
+    var correctAnswersCount: Int = 0,
+)
+
 fun main() {
     val wordsFile = File("words.txt")
     wordsFile.createNewFile()
-    wordsFile.appendText("hello привет\n")
-    wordsFile.appendText("dog собака\n")
-    wordsFile.appendText("cat кошка\n")
+    val dictionary = mutableListOf<Word>()
 
     wordsFile.readLines()
-        .forEach { word ->
-            println(word)
+        .forEach { line ->
+            val line = line.split("|")
+            val correctAnswersCount: Int = line[2].toIntOrNull() ?: 0
+            val word = Word(line[0], line[1], correctAnswersCount)
+            dictionary.add(word)
         }
+
+    dictionary.forEach { println(it) }
 }

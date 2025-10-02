fb84a1781ba647188754a9582e82e5ee13c705dc
APecherkin <pech.artyom2@yandex.ru>
2025-10-02 21:12:26 +0500

    KTB-08

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,6 +3,7 @@ package org.example
 import java.io.File
 
 const val CORRECT_ANSWERS_TO_LEARN = 3
+const val OPTIONS_COUNT = 4
 
 data class Word(
     val original: String,
@@ -25,9 +26,14 @@ fun main() {
         return dictionary
     }
 
-    val dictionary = loadDictionary()
+    fun saveDictionary(dictionary: List<Word>) {
+        val content = dictionary.joinToString("\n") { word ->
+            "${word.original}|${word.translate}|${word.correctAnswersCount}"
+        }
+        wordsFile.writeText(content)
+    }
 
-    dictionary.forEach { println(it) }
+    val dictionary = loadDictionary()
 
     while (true) {
         println(
@@ -40,7 +46,43 @@ fun main() {
         )
 
         when (readln()) {
-            "1" -> println("Учить слова")
+            "1" -> {
+                while (true) {
+                    val notLearnedList = dictionary.filter { it.correctAnswersCount < CORRECT_ANSWERS_TO_LEARN }
+                    if (notLearnedList.isEmpty()) {
+                        println("Все слова выучены!")
+                        continue
+                    }
+                    var questionWords = notLearnedList.shuffled().take(OPTIONS_COUNT)
+                    val questionWord = questionWords.random()
+                    if (questionWords.size < OPTIONS_COUNT) {
+                        val shortageCount = OPTIONS_COUNT - questionWords.size
+                        val shortageList =
+                            dictionary.filter { it.correctAnswersCount >= CORRECT_ANSWERS_TO_LEARN }.take(shortageCount)
+                        questionWords = questionWords.plus(shortageList)
+                    }
+                    val shuffledAnswers = questionWords.map { it.translate }.shuffled()
+                    val correctAnswerId = shuffledAnswers.indexOf(questionWord.translate) + 1
+                    println("${questionWord.original}:")
+                    for (i in 0 until OPTIONS_COUNT) {
+                        println("${i + 1} - ${shuffledAnswers[i]}")
+                    }
+                    println("----------\n" + "0 - Меню")
+                    val userAnswerInput = readln().toIntOrNull() ?: 0
+                    if (userAnswerInput == correctAnswerId) {
+                        println("Правильно!")
+                        dictionary[dictionary.indexOf(questionWord)].correctAnswersCount++
+                        saveDictionary(dictionary)
+                    } else if (userAnswerInput == 0) {
+                        break
+                    } else {
+                        println("Неправильно! ${questionWord.original} – это ${questionWord.translate}")
+                    }
+0
+                }
+
+            }
+
             "2" -> {
                 val totalCount = dictionary.size
                 val learnedCount = dictionary.filter { it.correctAnswersCount >= CORRECT_ANSWERS_TO_LEARN }.size
@@ -50,6 +92,7 @@ fun main() {
                     continue
                 } else println("Словарь пустой")
             }
+
             "0" -> break
             else -> println("Введите число 1, 2 или 0")
         }
